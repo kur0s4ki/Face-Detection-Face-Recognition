80f4fb15a6ef19c6d07b2cdabae251f6f228dbe1
kur0s4ki <54065821+kur0s4ki@users.noreply.github.com>
2020-02-21 18:01:32 +0100

    Add files via upload

--- Face-Recognition.py
+++ Face-Recognition.py
@@ -11,6 +11,7 @@ global Just_Enrolled
 known_face_encodings = []
 known_face_metadata = []
 ID_BD = []
+ID = 0
 
 # Parameters
 Tolerance = 0.6  # choose a value between 0 and 1 , The lower tolerance is ,the strict face recognition becomes.
@@ -57,44 +58,32 @@ def get_jetson_gstreamer_source(capture_width=1280, capture_height=720, display_
     )
 
 
-def get_ID():
+def get_ID(method):
     global ID_BD
+    global ID
     while True:
-        ID = str(uuid.uuid4())
-        # random = random.upper()
-        ID = ID.replace("-", "")
-        ID = ID[0:4]
-        if not (ID in ID_BD):
-            ID_BD.append(ID)
-            break
-    return ID
-
-
-def ID_counter():
-    global ID_BD
-    ID = 0
-    while True:
-        if ID in ID_BD:
+        if method == "UUID_Generator":
+            ID = str(uuid.uuid4())
+            ID = ID.replace("-", "")
+            ID = ID[0:4]
+            if not (ID in ID_BD):
+                ID_BD.append(ID)
+                break
+        elif method == "Counter":
+            if not (ID in ID_BD):
+                ID_BD.append(ID)
+                break
             ID += 1
-        else:
-            ID_BD.append(ID)
-            break
     return ID
 
 
 def register(face_encoding, face_image):
     face_image = cv2.resize(face_image, (75, 75))
     known_face_encodings.append(face_encoding)
-    if ID_Method == 'UUID_Generator':
-        known_face_metadata.append({
-            "id": get_ID(),
-            "face_image": face_image
-        })
-    else:
-        known_face_metadata.append({
-            "id": ID_counter(),
-            "face_image": face_image
-        })
+    known_face_metadata.append({
+        "id": get_ID(ID_Method),
+        "face_image": face_image
+    })
 
     return known_face_metadata[0]['id']
 
@@ -125,7 +114,7 @@ def updateEnroll(var: bool):
 
 def Calculate_Distance(top, right, bottom, left, method='Algebra'):
     if method == "Estimation":
-        distancei = (2 * 3.14 * 180) / ((right - left) + (bottom - top) * 360) * 1000
+        distancei = (2 * 3.14 * 180) / ((right - left) + (bottom - top) * 360) * 1000 + 3
         distance_in_cm = math.floor(distancei * 2.54)
     elif method == "Algebra":
         reye = left + ((right - left) / 2) - ((right - left) / 5)
@@ -144,8 +133,12 @@ def main_loop():
     face_counter = 0
     if running_on_jetson_nano():
         video_capture = cv2.VideoCapture(get_jetson_gstreamer_source(), cv2.CAP_GSTREAMER)
+        ret = video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 980)
+        ret = video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
     else:
         video_capture = cv2.VideoCapture(0)
+        ret = video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 980)
+        ret = video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
     while True:
         ret, frame = video_capture.read()
@@ -160,39 +153,32 @@ def main_loop():
         face_labels = []
         for face_location, face_encoding in zip(face_locations, face_encodings):
             metadata = find_faces(face_encoding)
-            Just_Enrolled = False  # type: bool
             if metadata is not None:
+                face_label = "Known"
                 top, right, bottom, left = face_location
                 dis = Calculate_Distance(top * 4, right * 4, bottom * 4, left * 4, method=Distance_Method)
                 if dis < Distance_Threshhold:
                     DummyFuncEnrolled(metadata['id'])
                     face_label = 'ID :{}'.format(metadata['id'])
-                else:
-                    face_label = 'far'
-                    print("\n")
+                face_labels.append(face_label)
+
             else:
                 face_label = "Unknown"
                 top, right, bottom, left = face_location
                 dis = Calculate_Distance(top * 4, right * 4, bottom * 4, left * 4, method=Distance_Method)
-                # print("dis = {}".format(dis))
                 if dis < Distance_Threshhold:
                     face_image = small_frame[top:bottom, left:right]
                     face_image = cv2.resize(face_image, (150, 150))
                     if Auto_Enrollment == "Enabled":
                         just = register(face_encoding, face_image)
                         DummyFuncUnknown(just)
-                        #updateEnroll()
                     else:
                         if cv2.waitKey(1) & 0xFF == ord('s'):
                             just = register(face_encoding, face_image)
                             DummyFuncUnknown(just)
-                            #updateEnroll()
-                            # break
                 else:
-                    print("\n")
-            #if Just_Enrolled:
-                #DummyFuncUnknown(metadata['id'])
-            face_labels.append(face_label)
+                    pass
+                face_labels.append(face_label)
 
         for (top, right, bottom, left), face_label in zip(face_locations, face_labels):
             top *= 4
@@ -210,15 +196,19 @@ def main_loop():
                     cv2.putText(frame, face_label, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8,
                                 (255, 255, 255), 1)
 
-                else:
+                elif 'ID' in face_label:
                     cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
                     cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
                     cv2.putText(frame, 'Distance : ' + str(distance_from_cam) + ' cm',
                                 (left + 140, top + (bottom - top) - 10), cv2.FONT_HERSHEY_DUPLEX, 0.5, (239, 246, 255))
                     cv2.putText(frame, face_label, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8,
                                 (255, 255, 255), 1)
+                else:
+                    pass
             else:
-                cv2.putText(frame, "GET YOUR FACE NEAR THE CAM", (200, 80), cv2.FONT_HERSHEY_DUPLEX, 1.2, (0, 0, 255),
+                width = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+                cv2.putText(frame, "GET YOUR FACE NEAR THE CAM", (int(width / 6), 80), cv2.FONT_HERSHEY_DUPLEX, 1.2,
+                            (0, 0, 255),
                             1)
 
         cv2.imshow('Video', frame)
